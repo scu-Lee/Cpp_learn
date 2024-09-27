85a9998ebd0c81757f233245cfed12d24521b77a
scu_Lee <scu_Lee@outlook.com>
2024-09-27 21:32:43 +0800

    布尔类型.cpp

---
+++ 2024_9_26/布尔类型.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+//int main()
+//{
+//	bool flag = true;
+//	cout << "flag = " << flag << endl;
+//	flag = false;
+//	cout << "flag = " << flag << endl;
+//	return 0;
+//}
